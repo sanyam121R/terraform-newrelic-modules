d92fe72edb665d19cca0778a05f1cd424dc65c0d
sanyam121R <sanyam.12rathore@gmail.com>
2023-07-14 13:04:48 +0530

    Updare

--- alerts/main.tf
+++ alerts/main.tf
@@ -7,8 +7,7 @@ resource "newrelic_alert_policy" "yan" {
 # USE THIS RESOURCE TO CREATE AND MANAGE NRQL ALERT CONDITIONS IN NEW RELIC.
 
 resource "newrelic_nrql_alert_condition" "sanyam" {
-  account_id = var.account_id
-  policy_id  = newrelic_alert_policy.yan.id
+  policy_id = newrelic_alert_policy.yan.id
 
   for_each    = var.alert_conditions
   name        = each.value.name
